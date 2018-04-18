c4a05a087b76a2cdc6f8b1978fb0b56db735aefd
Eric Osgood <ericthegamefreak@gmail.com>
2018-04-17 21:20:58 -0400

    Updated project to connect to database, no structure.

--- GEM - Core/Data/ApplicationDbContext.cs
+++ GEM - Core/Data/ApplicationDbContext.cs
@@ -15,6 +15,9 @@ namespace GEM.Data
         {
         }
 
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
--- GEM - Core/Models/Event.cs
+++ GEM - Core/Models/Event.cs
@@ -8,9 +8,12 @@ namespace GEM.Models
         public Guid Id { get; set; }
         public User Owner { get; set; }  
         public bool IsPrivate { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         public DateTimeOffset DateAndTime { get; set; }
+        [Required]
         public string Location { get; set; }
         public List<User> Attendees { get; set; }
         // public int Attendees { get; set; } //-------If doing attendee count, not actual list of attendees
